6a030eae79644be43a0105ac5b7b6e281c2305f8
Amith Pakittiri <amith0@yahoo.com>
2015-09-29 15:39:20 -0700

    ReactJs-.Net

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactJs_.Net.MvcApplication" Language="C#" %>
---
+++ Scripts/Tutorial.jsx
@@ -0,0 +1,54 @@
+﻿var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+          Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
+
+
+//var CommentBox = React.createClass({
+//    displayName: 'CommentBox',
+//    render: function () {
+//        return (
+//          React.createElement('div', { className: "commentBox" },
+//            "Hello, world! I am a CommentBox."
+//          )
+//        );
+//    }
+//});
+//React.render(
+//  React.createElement(CommentBox, null),
+//  document.getElementById('content')
+//);
